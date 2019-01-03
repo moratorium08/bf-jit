8ecea2bf770a52bdfdb760b4aeabc315d7a227f9
moratorium08 <moratorium08@gmail.com>
2019-01-04 01:21:56 +0900

    Fix bench script

--- bench.py
+++ bench.py
@@ -1,23 +1,42 @@
 import subprocess
 
-count = 1
+count = 5
 
 jit_command = './bench/jit.sh'
 nojit_command = './bench/nojit.sh'
 bfi_command = './bench/bfi.sh'
+bff4_command = './bench/bff4.sh'
 
 commands = [
-        jit_command,
-        nojit_command,
-        bfi_command,
+        #jit_command,
+        #nojit_command,
+        #bfi_command,
+        bff4_command
         ]
 
+l = []
 for command in commands:
-    cnt = 0.0
+    print(command)
+    tmp = []
     for i in range(count):
+        print(i)
         r = subprocess.check_output(['/usr/bin/time','-f', '%e', command] ,
                 stderr=subprocess.STDOUT, shell=False)
         s = r.rstrip('\n')
-        cnt += float(s)
+        tmp.append(float(s))
+    l.append((command.split('/')[-1].strip('.sh'), tmp))
 
-    print(command.split('/')[-1].strip('.sh'), cnt / count)
+for x, y in l:
+    print(x)
+    m = 0.0
+    for z in y:
+        m += z
+    m /= count
+    print(m)
+    v = 0.0
+    for z in y:
+        v += (z - m) ** 2
+    v /= count
+    print(v)
+
+print(l)
